c9f87c8ecd5ac18596e72238172961cf35388baf
Nick Yeung <nickyeung425@hotmail.com>
2021-05-18 16:48:24 +0800

    Added react-navigation as routing services for the app

--- App.js
+++ App.js
@@ -8,20 +8,43 @@
 
 import React from 'react';
 import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
-import {withAuthenticator} from 'aws-amplify-react-native';
+// import {withAuthenticator} from 'aws-amplify-react-native';
 import {Container, Text} from 'native-base';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {View, Button} from 'react-native';
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Container>
-          <Text>Using native-base</Text>
-        </Container>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-export default withAuthenticator(App);
+// export default withAuthenticator(App);
+export default App;
